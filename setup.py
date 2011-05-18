25e5a03fe5fc05868e88a895c0a53b45c469aa4a
Tim Henderson <tim.tadh@gmail.com>
2011-05-18 11:34:25 -0400

    added setup script
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='swork',
+      version='0.1',
+      description='A shell enviroment manager. Allows you to easily manage the shell enviroment for
+      working on various projects.',
+      author='Tim Henderson',
+      author_email='tim.tadh@gmail.com',
+      url='https://www.github.com/timtadh/swork',
+      packages=['swork'],
+)
