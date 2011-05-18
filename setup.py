686803dfc0461d942c49375d28f8377c36779f2a
Tim Henderson <tim.tadh@gmail.com>
2011-05-18 11:43:53 -0400

    work on building
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from distutils.core import setup
 
 setup(name='swork',
       version='0.1',
-      description='A shell enviroment manager. Allows you to easily manage the shell enviroment for
-      working on various projects.',
+      description=(
+        'A shell enviroment manager. Allows you to easily manage the shell enviroment for'
+        'working on various projects.'
+      ),
       author='Tim Henderson',
       author_email='tim.tadh@gmail.com',
       url='https://www.github.com/timtadh/swork',
-      packages=['swork'],
+      license='GPLv2',
+      packages=['sworklib'],
+      scripts=['bin/swork'],
+      platforms=['unix']
 )
